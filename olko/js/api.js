b1aa81a365bc9defb9a40b7f236c0ee808a23a66
ollyolko <olly.olko@gmail.com>
2017-05-14 00:59:46 +0300

    -js

---
+++ olko/js/api.js
@@ -0,0 +1,22 @@
+$.getJSON('http://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=4bff6b538cde4e5d8e6797f89015c017', function(data) {
+    console.log(data);
+    var items = [];
+    $.each(data.articles, function (key, val) {
+        items.push('' +
+            '<div class="col-sm-6 col-md-4 col-lg-3 api">' +
+            '<h3>'+val.title+'</h3>' +
+            '<img src="'+val.urlToImage+'">' +
+            '<p>'+val.description+'</p>' +
+            '<a href="'+val.url+'">...read more.</a>' +
+            '<p>'+val.author+'  '+val.publishedAt+'</p>'+
+            '</div>');
+
+    });
+
+    $('<p/>', {
+        'class': 'my-new-list',
+        html: items.join('')
+    }).appendTo('.min');
+});
+
+

ca2ea2b2a35c14a8f862f9797868325e94ab90f5
ollyolko <olly.olko@gmail.com>
2017-05-16 21:30:07 +0300

    api function

--- olko/js/api.js
+++ olko/js/api.js
@@ -1,5 +1,6 @@
-$.getJSON('http://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=4bff6b538cde4e5d8e6797f89015c017', function(data) {
-    console.log(data);
+var str = 'http://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=4bff6b538cde4e5d8e6797f89015c017';
+
+var ansver = function (data) {
     var items = [];
     $.each(data.articles, function (key, val) {
         items.push('' +
@@ -10,13 +11,15 @@ $.getJSON('http://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=4bff6b
             '<a href="'+val.url+'">...read more.</a>' +
             '<p>'+val.author+'  '+val.publishedAt+'</p>'+
             '</div>');
-
+        return items;
     });
 
+    push(items);
+};
+
+function push(items) {
     $('<p/>', {
         'class': 'my-new-list',
         html: items.join('')
     }).appendTo('.min');
-});
-
-
+}

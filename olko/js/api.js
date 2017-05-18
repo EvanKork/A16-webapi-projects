7fedd166eaabd690b7b38e89fd7a46bb0597c761
ollyolko <olly.olko@gmail.com>
2017-05-19 01:20:44 +0300

    api function, .idea

--- olko/js/api.js
+++ olko/js/api.js
@@ -1,25 +1,28 @@
 var str = 'http://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=4bff6b538cde4e5d8e6797f89015c017';
 
-var ansver = function (data) {
+function createItem(data) {
+    var blockItem = $('<div/>').addClass('col-sm-6 col-md-4 col-lg-3 api');
+    var title = $('<h3/>').text(data.title);
+    var img = $('<img src="'+data.urlToImage+'"/>');
+    var pDescr = $('<p/>').text(data.description);
+    var a = $('<a href="'+data.url+'"/>').text('read more...');
+    var pAuthor = $('<p/>').text(data.author);
+    var date = $('<p/>').text(data.publishedAt);
+    blockItem.append(title).append(img).append(pDescr).append(a).append(pAuthor).append(date);
+    return blockItem;
+}
+
+
+function createItems(data) {
     var items = [];
     $.each(data.articles, function (key, val) {
-        items.push('' +
-            '<div class="col-sm-6 col-md-4 col-lg-3 api">' +
-            '<h3>'+val.title+'</h3>' +
-            '<img src="'+val.urlToImage+'">' +
-            '<p>'+val.description+'</p>' +
-            '<a href="'+val.url+'">...read more.</a>' +
-            '<p>'+val.author+'  '+val.publishedAt+'</p>'+
-            '</div>');
-        return items;
+        items.push(createItem(val));
     });
+    return items;
+}
 
-    push(items);
+var answer = function (data) {
+    $('.min').append(createItems(data))
 };
 
-function push(items) {
-    $('<p/>', {
-        'class': 'my-new-list',
-        html: items.join('')
-    }).appendTo('.min');
-}
+$.getJSON(str,answer);
